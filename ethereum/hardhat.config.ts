b181b6f5d0a30aa3304c8f328375b65c19f12d81
Fantasista766 <proxxx766@gmail.com>
2024-11-03 18:42:49 +0300

    fix hardhat config

--- ethereum/hardhat.config.ts
+++ ethereum/hardhat.config.ts
@@ -1,8 +1,24 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+
+//npx typechain --target ethers-v6 --out-dir ./front/src/typechain "artifacts/contracts/**/*[!dbg].json"
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+      initialBaseFeePerGas: 0,
+    },
+    sepolia: {
+      url: `${process.env.ALCHEMY_SEPOLIA_URL}`,
+      accounts: [`0x${process.env.SEPOLIA_PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    apiKey: `${process.env.ETHERSCAN_KEY}`,
+  },
 };
 
 export default config;
